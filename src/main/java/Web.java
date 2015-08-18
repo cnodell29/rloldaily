b0110863065b3edc732360e9851757b52ea12dbf
Cory Odell <novijags29@aol.com>
2015-08-18 00:34:01 -0400

    I think I got the ftl template working

--- src/main/java/Web.java
+++ src/main/java/Web.java
@@ -8,6 +8,9 @@ import spark.template.freemarker.FreeMarkerEngine;
 
 public class Web {
 	public static void main(String[] args) {
+		
+		port(Integer.valueOf(System.getenv("PORT")));
+
 		get("/", (req, res) -> {
 			Map<String, Object> attributes = new HashMap<>();
 			attributes.put("message", "Welcome to the daily generated r/leagueoflegends youtube playlist!");
